91d5d30181034113d6a700b24b3f94de523b6691
Amir Hossein Peyvand <amirhosseinpeyvand2020@gmail.com>
2023-12-19 12:56:55 -0800

    Styling login link and register button for desktop

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,23 +3,28 @@ import menu from "../assets/icon-menu.svg";
 import logo from "../assets/logo.svg";
 import "../sass/Navbar.scss";
 import MobileMenu from "./MobileMenu";
+import { useMediaQuery } from "react-responsive";
 import DesktopNavItems from "./DesktopNavItems";
 
 const Navbar = () => {
   const [clicked, setClicked] = useState(false);
+  const isGreaterThan800px = useMediaQuery({ query: "(min-width:800px)" });
+
+  const menuLogoContainer = (
+    <section className="menuLogoContainer">
+      {!clicked && (
+        <img src={menu} alt="menu" onClick={() => setClicked(true)} />
+      )}
+      {clicked && <MobileMenu onSetClicked={() => setClicked(false)} />}
+    </section>
+  );
 
   return (
     <nav className="navbar">
       <section className="logoContainer">
         <img src={logo} alt="logo" />
       </section>
-      {/* <section className="menuLogoContainer">
-        {!clicked && (
-          <img src={menu} alt="menu" onClick={() => setClicked(true)} />
-        )}
-      </section> */}
-      <DesktopNavItems />
-      {clicked && <MobileMenu onSetClicked={() => setClicked(false)} />}
+      {isGreaterThan800px ? <DesktopNavItems /> : menuLogoContainer}
     </nav>
   );
 };
